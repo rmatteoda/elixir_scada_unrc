5629a2e6dbe8443182b8a7e4ad0cd8fad5ae02b6
Ramiro Matteoda <ramiro.matteoda@3sharecorp.com>
2016-07-31 14:56:21 -0300

    add collector time parameter

--- scada_master/lib/device/scheduler.ex
+++ scada_master/lib/device/scheduler.ex
@@ -2,13 +2,16 @@ defmodule SCADAMaster.Device.Scheduler do
   use GenServer
   require Logger
 
+# configure time in ms to collect data from scada devies.
+  @collect_time 30000
+
   def start_link do
     GenServer.start_link(__MODULE__, %{})
   end
 
   def init(loader) do
     Logger.debug "Start System from Scheduler handler " 
-    Process.send_after(self(), :work, 15 * 1000) # In 1 minute - ver de agregar tiempo configurado
+    Process.send_after(self(), :work, @collect_time) 
     SCADAMaster.Device.Loader.start_link       
   end
 
@@ -17,7 +20,7 @@ defmodule SCADAMaster.Device.Scheduler do
     SCADAMaster.Device.Loader.load(loader);
 
     # Start the timer again
-    Process.send_after(self(), :work, 30 * 1000) # In 1 minute - ver de agregar tiempo configurado
+    Process.send_after(self(), :work, @collect_time) 
     {:noreply, loader}
   end
 end
