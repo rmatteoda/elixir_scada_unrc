0ba65d2660a322939d5a747cfe94f501843a5f08
Ramiro Matteoda <ramiro.matteoda@3sharecorp.com>
2016-11-01 17:07:44 -0300

    Add migrations table to create on reset db running mix ecto.reset

---
+++ scada_master/priv/repo/migrations/20161025112541_create_tables.exs
@@ -0,0 +1,38 @@
+defmodule Repo.CreateTables do
+  use Ecto.Migration
+
+  def up do
+    create table(:device) do
+      add :substation_id, :integer
+      add :voltage_a,   :float
+      add :voltage_b,   :float
+      add :voltage_c,   :float
+      add :current_a, :float
+      add :current_b, :float
+      add :current_c, :float
+      add :activepower_a, :float
+      add :activepower_b, :float
+      add :activepower_c, :float
+      add :reactivepower_a, :float
+      add :reactivepower_b, :float
+      add :reactivepower_c, :float
+      add :totalactivepower, :float
+      add :totalreactivepower, :float
+      add :unbalance_voltage, :float
+      add :unbalance_current, :float
+      timestamps
+    end
+    create index(:device, [:substation_id])
+
+    create table(:substations) do
+      add :name, :string, size: 40, null: false, unique: true
+    end
+
+    create unique_index(:substations, [:name], name: :unique_names)
+  end
+
+  def down do
+    drop index(:device, [:substation_id])
+    drop table(:substations)
+  end
+end
